fa78795d67b145f0e59d77cdcf3bcd8d673c82a5
Emily Henry <emilyhen@usc.edu>
2020-01-02 19:53:10 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Emily Henry on 1/2/20.
+//  Copyright © 2020 Emily Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
